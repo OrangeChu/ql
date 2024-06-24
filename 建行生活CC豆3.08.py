c558c9aa56d4a67259d2e20ec5ea29f85c92f716
OrangeChu <757581542@qq.com>
2024-06-25 07:16:09 +0800

    Update 建行生活CC豆3.08.py

--- 建行生活CC豆3.08.py
+++ 建行生活CC豆3.08.py
@@ -1106,6 +1106,7 @@ if __name__ == "__main__":
 
     for i, cookie in enumerate(cookies, start = 1):
         print(f"\n======== ▷ 第 {i} 个账号 ◁ ========")
+        time.sleep(random.randint(60, 6000))
         CCD(cookie).auth_login()
         print("\n随机等待5-10s进行下一个账号")
         time.sleep(random.randint(5, 10))
